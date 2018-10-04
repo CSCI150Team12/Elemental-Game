c3117e6613e923ab8c733c9d3280f0ef9136a44f
Daniel Wright <wrightdj2000@gmail.com>
2018-10-04 15:16:16 -0700

    Organize file structure

---
+++ Assets/Scripts/Spells/EarthSpell.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthSpell : Spell
+{
+	
+	// Update is called once per frame
+	public override void Update () {
+        base.Update();
+        if (dying)
+        {
+            Slide(-5f, -1.1f, 1);
+        }
+        else
+        {
+            Slide(5f, -1.1f, 1);
+        }
+        
+	}
+
+    void Slide(float speed, float min, float max)
+    {
+        if ((transform.position.y < max && speed > 0) || (transform.position.y > min && speed < 0 ))
+        {
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        }
+    }
+}
