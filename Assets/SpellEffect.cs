b26d78b7ebe5107efedb6bef3c769846e8ecabe5
Shaunt Der Haroutunian <shauntdh@gmail.com>
2018-11-01 16:17:16 -0700

    Revert "Revert "fireball added""
    This reverts commit a8fd9e2f37caf63795e30a47d0fafea3a27c3e1e.

---
+++ Assets/SpellEffect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellEffect : MonoBehaviour {
+
+    public float damagePerSecond = 0f;
+    public float lifespan = 1f;
+    public float deathspan = 1f;
+    private float startTime;
+
+	void Start () {
+        startTime = Time.time;
+        InvokeRepeating("DealDamage", 1f, 0.1f);
+	}
+	
+	void Update () {
+        if (startTime + lifespan < Time.time)
+        {
+            Die();
+        }
+	}
+
+    void DealDamage()
+    {
+        PlayerController player = gameObject.GetComponentInParent<PlayerController>();
+        if (player)
+        {
+            player.TakeDamage(damagePerSecond / 10);
+        }
+    }
+
+    public void Die()
+    {
+        GetComponent<ParticleSystem>().Stop();
+        CancelInvoke();
+        Destroy(gameObject, deathspan);
+    }
+}
