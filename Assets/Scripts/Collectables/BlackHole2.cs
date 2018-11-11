d564710ee1ec622875fcb5a41b8d1f503d5ea806
RussellSchellenberg <GitHub@cs05rivu2spu.33mail.com>
2018-11-11 13:10:59 -0800

    Fixed to work with the current master
    Health and Speed Potion Prefabs, as well as black hole collectable is ready

---
+++ Assets/Scripts/Collectables/BlackHole2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole2 : MonoBehaviour {
+    public GameObject pickupSpell;     // Creates the game object
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))    // If the player touches the item, then activate!
+        {
+            StartCoroutine(PickupBlackHole(other));  // StartCoroutine because of the timer
+        }
+    }
+
+    IEnumerator PickupBlackHole(Collider player)
+    {
+        Instantiate(pickupSpell, transform.position, transform.rotation);
+
+        PlayerController PC = player.GetComponent<PlayerController>();  // Obtain "PlayerController", properties
+
+
+
+        PC.spellStr = "Black Hole Spell";      // Changed speed to 8
+
+        GetComponent<MeshRenderer>().enabled = false;   //Makes the item disappear
+
+        GetComponent<Collider>().enabled = false;                       //Makes the item disapper
+
+        yield return new WaitForSeconds(0);     // Timer, (KEEP FOR A RETURN VALUE)
+
+        Destroy(gameObject);    // Gets rid of the object
+    }
+}
---
+++ Assets/Scripts/Collectables/HealthPotionScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Removes 50 damage from Player
+
+public class HealthPotionScript : MonoBehaviour
+{
+
+    public GameObject pickupEffect;     // Creates the game object
+    private float holder;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))    // If the player touches the item, then activate!
+        {
+            StartCoroutine(Pickup(other));  // StartCoroutine because of the timer
+        }
+    }
+
+    IEnumerator Pickup(Collider player)
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        PlayerController PC = player.GetComponent<PlayerController>();  // Obtain "PlayerController", properties
+
+
+
+        holder = PC.damage;      // Moves damage into placeholder
+
+        if (holder <= 50)       // If the user has less than 50 damage
+        {
+            PC.damage = 0;      // Then damge = 0
+            PC.TakeDamage(0);
+        }
+        else                    // Else remove 50 damage from current damage
+        {
+            PC.TakeDamage(-50f);   
+        }
+
+        GetComponent<MeshRenderer>().enabled = false;   //Makes the item disappear
+
+        GetComponent<Collider>().enabled = false;          //Makes the item disapper
+
+        yield return new WaitForSeconds(0);     // Timer
+
+        Destroy(gameObject);    // Gets rid of the object
+    }
+}
