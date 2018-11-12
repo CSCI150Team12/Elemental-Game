ae4242c9d579f5e7f3e82425d9325cfad2c3d636
Jovani Gonzalez <jovani@mail.fresnostate.edu>
2018-11-12 02:40:32 -0800

    Implement main menu button sfx

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+
+    public AudioSource Button;
+
+    // Play button sound
+    public void PlayButton()
+    {
+        Button.Play();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
