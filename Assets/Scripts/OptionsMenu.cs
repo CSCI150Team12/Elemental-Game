f4ba09dcdba7e5c05157373d0cef3f3f92f031b2
Jovani Gonzalez <jovani@mail.fresnostate.edu>
2018-11-20 21:33:22 -0800

    Implemented options menu
    Currently accessible from main menu.

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour {
+
+    public GameObject optionsMenuUI;
+
+    // Go back to main menu
+    public void goBack()
+    {
+        optionsMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    // Show options menu
+    public void showOptions()
+    {
+        optionsMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
