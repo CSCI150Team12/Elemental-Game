a8cc0ae3c9b7d2b0aa318ce89a6c9cd478293f2d
RussellSchellenberg <GitHub@cs05rivu2spu.33mail.com>
2018-11-23 16:24:43 -0800

    Added Dark Collectable/Spell

---
+++ Assets/DLScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DLScript : MonoBehaviour {
+
+    public bool LightSwitch = true;
+    private Light myLight;
+	void Start ()
+    {
+        myLight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        myLight.enabled = GlobalVariables.LightSwitch;
+    }
+}
---
+++ Assets/Scripts/Spells/DarkSpell.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DarkSpell : MonoBehaviour
+{
+    public float duration;
+    private void Start()
+    {
+        GlobalVariables.LightSwitch = false;
+    }
+
+    private void Update()
+    {
+        duration -= Time.deltaTime;
+        if (duration <= 0)
+            GlobalVariables.LightSwitch = true;
+    }
+
+}
