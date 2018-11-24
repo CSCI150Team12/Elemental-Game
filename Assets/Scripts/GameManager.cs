925d9a18b0a47430cc1586bed0c175ba95376d10
Jovani Gonzalez <jovani@mail.fresnostate.edu>
2018-11-24 13:34:57 -0800

    Added persistence to user defined settings

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    // Don't destroy 'ManagerObject' on load
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // Load volume settings
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            Debug.Log("HasMKEY");
+            audioMixer.SetFloat("MusicFaderVolume", PlayerPrefs.GetFloat("MusicVolume"));
+        }
+
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            Debug.Log("HasSKEY");
+            audioMixer.SetFloat("SFXFaderVolume", PlayerPrefs.GetFloat("SFXVolume"));
+        }
+
+        if (PlayerPrefs.HasKey("UIVolume"))
+        {
+            Debug.Log("HasUKEY");
+            audioMixer.SetFloat("UIFaderVolume", PlayerPrefs.GetFloat("UIVolume"));
+        }
+
+        // Load main menu (next scene)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/Menus/OptionsMenu.cs
+++ Assets/Scripts/Menus/OptionsMenu.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public GameObject optionsMenuUI;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    public Slider uiSlider;
+    public float musicVar;
+    public float sfxVar;
+    public float uiVar;
 
     // Hide options menu
     public void HideOptions()
     {
+        // Save settings
+        PlayerPrefs.SetFloat("MusicVolume", musicVar);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVar);
+        PlayerPrefs.SetFloat("UIVolume", uiVar);
+
         optionsMenuUI.SetActive(false);
         Time.timeScale = 1f;
     }
@@ -19,18 +31,21 @@ public class OptionsMenu : MonoBehaviour
     public void SetMusicVolume(float musicVolume)
     {
         audioMixer.SetFloat("MusicFaderVolume", musicVolume);
+        musicVar = musicVolume;
     }
 
     // Set SFX volume
     public void SetSFXVolume(float sfxVolume)
     {
         audioMixer.SetFloat("SFXFaderVolume", sfxVolume);
+        sfxVar = sfxVolume;
     }
 
     // Set UI volume
     public void SetUIVolume(float uiVolume)
     {
         audioMixer.SetFloat("UIFaderVolume", uiVolume);
+        uiVar = uiVolume;
     }
 
     // Show options menu
@@ -38,6 +53,28 @@ public class OptionsMenu : MonoBehaviour
     {
         optionsMenuUI.SetActive(true);
         Time.timeScale = 0f;
+
+        // Load volume settings
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            musicVar = PlayerPrefs.GetFloat("MusicVolume");
+            audioMixer.SetFloat("MusicFaderVolume", musicVar);
+            musicSlider.value = musicVar;
+        }
+
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            sfxVar = PlayerPrefs.GetFloat("SFXVolume");
+            audioMixer.SetFloat("SFXFaderVolume", sfxVar);
+            sfxSlider.value = sfxVar;
+        }
+
+        if (PlayerPrefs.HasKey("UIVolume"))
+        {
+            uiVar = PlayerPrefs.GetFloat("UIVolume");
+            audioMixer.SetFloat("UIFaderVolume", uiVar);
+            uiSlider.value = uiVar;
+        }
     }
 
     // Use this for initialization
