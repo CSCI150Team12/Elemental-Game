96e6e465ec66723326eea37b97d0b5db0a858306
Wright-Dj <wrightdj2000@gmail.com>
2018-12-04 21:31:27 -0800

    Fix merge errors

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -3,47 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
-
-    public GameObject mainMenuUI;
-
-    // Load 'Game' scene
-    public void PlayGame()
-    {
-        StartCoroutine("DelayTime");
-        Time.timeScale = 1;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        Time.timeScale = 1f;
+//Button management class for the main menu scene
+public class MainMenu : MonoBehaviour {
+    // Play game button function
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Loads the game scene
+        Time.timeScale = 1f;                                                  // Unpauses the game 
     }
-
-    IEnumerator DelayTime()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(1);
-        }
-    }
-
-    // Hide main menu
-    public void HideMainMenu()
-    {
-        mainMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-    }
-
-    // Show main menu
-    public void ShowMainMenu()
-    {
-        mainMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-    }
-
-    // Quit game
-    public void QuitGame()
-    {
+    // Quit button function
+    public void QuitGame() {
 
         Debug.Log("QUIT!");
-        Application.Quit();
+        Application.Quit(); // Quits the application
     }
 }
